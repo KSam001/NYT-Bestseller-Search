9fd954a620ba80d49d5c056e1f7a00ccbcfc888d
KSam001 <111123481+KSam001@users.noreply.github.com>
2026-05-30 00:12:53 +0300

    part 2

--- seller_functions.py
+++ seller_functions.py
@@ -2,11 +2,37 @@ def yearRange(books:list[dict]):
    beginning_year = int(input("Enter beginning year: "))
    ending_year = int(input("Enter ending year: "))
 
+
    for book in books:
       year = book.get("date").split("/")[2]
 
       if int(year) >= beginning_year and int(year) <= ending_year:
          print(f'{book.get("title")}, by {book.get("author")} ({book.get("date")})')
+
+def monthYear(books):
+   input_month = int(input("Enter month of year in number: "))
+   input_year = int(input("Enter year: "))
+
+   match_found = False
+
+   for book in books:
+      date_part = book.get("date").split("/")
+      
+      month = int(date_part[0])
+      year = int(date_part[2])
+
+      if month == input_month and year == input_year:
+         print(f'{book.get("title")}, by {book.get("author")} ({book.get("date")})')
+         match_found = True
+
+
+
+   
+
+
+
+
+
 
       
 
